c40b02805553b9e20b99ec7451090166cb0c4694
Vijay Kapse <vj@Vijays-MacBook-Air.local>
2026-06-03 13:27:37 -0400

    Allow RMS login over sysrev2 http path

--- runtime-manager/bin/start-portal.sh
+++ runtime-manager/bin/start-portal.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 cd /home/vkapse/vijay-portal
 export PUBLIC_PATH_PREFIX="${PUBLIC_PATH_PREFIX:-/rms}"
-export PUBLIC_BASE_URL="${PUBLIC_BASE_URL:-https://sysrev2.cs.binghamton.edu/rms}"
-export COOKIE_SECURE="${COOKIE_SECURE:-true}"
+export PUBLIC_BASE_URL="${PUBLIC_BASE_URL:-http://sysrev2.cs.binghamton.edu/rms}"
+export COOKIE_SECURE="${COOKIE_SECURE:-false}"
 exec node server.js
